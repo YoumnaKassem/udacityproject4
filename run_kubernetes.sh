1a7409ab9214fb644c7824e150034b5334bd81d2
youmnakassem <youmna.kasem@gmail.com>
2021-09-23 16:17:17 +0200

    adding circle ci

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,20 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="01123555566/api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run yoyo\
+    --image=$dockerpath\
+    --port=80 --labels yoyo
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward latest 8000:80
 
